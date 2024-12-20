3deacc30c4fb0e75b4caf566469183dd66a1776e
Daniel Reis <dreis@opensourceintegrators.com>
2024-12-20 13:33:35 +0000

    [FIX] l10n_pt_vat: error when searching VAT Reason by name

--- l10n_pt_vat/models/l10n_pt_vat_exempt_reason.py
+++ l10n_pt_vat/models/l10n_pt_vat_exempt_reason.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-from odoo.osv import expression
 
 
 class VatExemptReason(models.Model):
@@ -15,24 +14,7 @@ class VatExemptReason(models.Model):
     active = fields.Boolean(default=True)
     note = fields.Text(string="Description")
 
-    def name_get(self):
-        return [(x.id, f"[{'x.code'}] {'x.name'}") for x in self]
-
-    @api.model
-    def _name_search(
-        self, name, args=None, operator="ilike", limit=100, name_get_uid=None
-    ):
-        """
-        Returns a list of tuples containing id, name,
-        as internally it is called <<def name_get>>
-        result format: {[(id, name), (id, name), ...]}
-        """
-        args = args or []
-        if operator == "ilike" and not (name or "").strip():
-            domain = []
-        else:
-            connector = "&" if operator in expression.NEGATIVE_TERM_OPERATORS else "|"
-            domain = [connector, ("code", operator, name), ("name", operator, name)]
-        return self._search(
-            expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid
-        )
+    @api.depends("code", "name")
+    def _compute_display_name(self):
+        for rec in self:
+            rec.display_name = f"[{rec.code}] {rec.name}"
