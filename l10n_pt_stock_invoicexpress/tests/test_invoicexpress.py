ac61daf3319fe3e00dc2d5998b0dd8c6019ce062
EmilioPascual <emilio@moduon.team>
2025-03-05 09:46:54 +0100

    [FIX] l10n_pt_stock_invoicexpress: test doesn't load Coa
    After this commit https://github.com/odoo/odoo/commit/d0342c8 the Coa is not loaded in the test, so we need to load it manually.

--- l10n_pt_stock_invoicexpress/tests/test_invoicexpress.py
+++ l10n_pt_stock_invoicexpress/tests/test_invoicexpress.py
@@ -23,6 +23,16 @@ def mock_response(json, status_code=200):
 class TestInvoiceXpressStock(TestInvoiceXpress):
     def setUp(self):
         super().setUp()
+        if not self.env.company.chart_template_id:
+            # Load a CoA if there's none in current company
+            coa = self.env.ref("l10n_generic_coa.configurable_chart_template", False)
+            if not coa:
+                # Load the first available CoA
+                coa = self.env["account.chart.template"].search(
+                    [("visible", "=", True)], limit=1
+                )
+            coa.try_loading(company=self.env.company, install_demo=False)
+
         self.StockPicking = self.env["stock.picking"]
 
     @patch.object(requests, "request")
