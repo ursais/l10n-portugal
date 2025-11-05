e6b6f47049e934d553135b2345178e32e5049053
Daniel Reis <dreis@opensourceintegrators.com>
2025-11-05 10:21:17 +0000

    [ADD] l10n_pt_future: backport of l10n_pt future changes
    Odoo has WIP towards supporting Portuguese legal invoicing, here:
    https://github.com/odoo/odoo/pull/196102
    
    The APO is working on a local and shared localization,
    and is relying on these future changes.
    
    To be able to test and deploy now, we are providing backports of the
    needed features as "_future" modules.

---
+++ l10n_pt_future/models/template_pt.py
@@ -0,0 +1,67 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models
+from odoo.addons.account.models.chart_template import template
+
+
+class AccountChartTemplate(models.AbstractModel):
+    _inherit = 'account.chart.template'
+
+    @template('pt')
+    def _get_pt_template_data(self):
+        return {
+            'property_account_receivable_id': 'chart_2111',
+            'property_account_payable_id': 'chart_2211',
+            'property_account_income_categ_id': 'chart_711',
+            'property_account_expense_categ_id': 'chart_311',
+        }
+
+    @template('pt', 'res.company')
+    def _get_pt_res_company(self):
+        return {
+            self.env.company.id: {
+                'account_fiscal_country_id': 'base.pt',
+                'bank_account_code_prefix': '12',
+                'cash_account_code_prefix': '11',
+                'transfer_account_code_prefix': '1431',
+                'account_default_pos_receivable_account_id': 'chart_2117',
+                'income_currency_exchange_account_id': 'chart_7861',
+                'expense_currency_exchange_account_id': 'chart_6863',
+                'account_journal_early_pay_discount_loss_account_id': 'chart_682',
+                'account_journal_early_pay_discount_gain_account_id': 'chart_728',
+                'tax_calculation_rounding_method': 'round_globally',
+                'account_sale_tax_id': 'iva_pt_sale_normal',
+                'account_purchase_tax_id': 'iva_pt_purchase_normal',
+            },
+        }
+
+    @template(model='account.journal')
+    def _get_account_journal(self, template_code):
+        vals = super()._get_account_journal(template_code)
+        if template_code == 'pt':
+            if 'cash' in vals:
+                vals['cash']['default_account_id'] = 'chart_11'
+            if 'bank' in vals:
+                vals['bank']['default_account_id'] = 'chart_12'
+        return vals
+
+    @template('pt', 'account.journal')
+    def _get_pt_account_account_journal(self):
+        """
+        Different AT Series are needed for invoices and refunds. For consistency, have a dedicated sequence
+        for refunds by default.
+        """
+        return {
+            'sale': {
+                'refund_sequence': True,
+            },
+        }
+
+    def _load(self, template_code, company, install_demo, force_create=True):
+        """
+        Set tax calculation rounding method required in the Portuguese localization to
+        prevent rounding errors according to the requirements of the Autoridade Tributaria
+        """
+        res = super()._load(template_code, company, install_demo, force_create)
+        if company.account_fiscal_country_id.code == 'PT':
+            company.write({'tax_calculation_rounding_method': 'round_globally'})
+        return res
